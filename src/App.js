bdf642d9d13d3492143d2ca49744b3a513ebb92f
ronantr <noob93500@gmail.com>
2022-06-29 10:38:04 +0200

    add firebase + config

--- src/App.js
+++ src/App.js
@@ -3,7 +3,51 @@ import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
 
+
 function App() {
+  /*var quill1 = new Quill('#editor-container', {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block']
+      ]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow'  // or 'bubble'
+  });
+  var quill2 = new Quill('#editor-container1', {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block']
+      ]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow'  // or 'bubble'
+  });
+  
+  var quill3 = new Quill('#editor-container2', {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block']
+      ]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow'  // or 'bubble'
+  });
+
+*/
+  const [text, setText] = useState();
+
+  const handleclick = (e) =>{
+
+console.log("tefegefegg",e)
+    setText(e.target.value)
+  }
   return (
     <Router>
       <div className="App">
@@ -14,7 +58,17 @@ function App() {
         </Routes>
       </div>
     </Router>
+
   );
-}
+  
+}    
+/*<div id="editor-container">
+</div>
+
+<div id="editor-container1">
+</div>
+
+<div id="editor-container2">
+</div>*/
 
 export default App;
