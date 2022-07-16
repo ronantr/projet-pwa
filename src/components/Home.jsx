e8c2565f20d27f8e0ccb65b9c6602d1608272b00
ronantr <noob93500@gmail.com>
2022-07-16 15:29:17 +0200

    add reveal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 import { auth } from '../service/firebase'
 import '../App.css';
+import Reveal from 'reveal.js';
+
 
 function Home({user}) {
   console.log(user);  
+
   return (
+
     <div className="home">
+
+<link rel="stylesheet" href="/node_modules/reveal.js/dist/reveal.css"/>
+<link rel="stylesheet" href="/node_modules/reveal.js/dist/theme/black.css"/>
     <h1>Hello, <span></span>{user.displayName}</h1>
     <h2>{user.email}</h2>
     <h2>{user.uid}</h2>
@@ -14,6 +21,7 @@ function Home({user}) {
     <img src={user.photoURL} alt="" />
     <p></p>
     <button className="button signout" onClick={() => auth.signOut()}>Sign out</button>
+    
   </div>
     
   )
